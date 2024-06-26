15141f1e35ddd8ae5652e741d993096cb2e08aaa
fengzhengyi <512369179@qq.com>
2024-06-26 23:16:22 +0800

    rails g scaffold band name description:text genre_tags:text

--- db/migrate/20240626145115_create_venues.rb
+++ db/migrate/20240626145115_create_venues.rb
@@ -2,7 +2,7 @@ class CreateVenues < ActiveRecord::Migration[7.1]
   def change
     create_table :venues do |t|
       t.string :name
-      t.string :description
+      t.text :description
       t.integer :rows
       t.integer :seats_per_row
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,9 +10,17 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2024_06_26_145115) do
+ActiveRecord::Schema[7.1].define(version: 2024_06_26_150704) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "bands", force: :cascade do |t|
+    t.string "name"
+    t.text "description"
+    t.text "genre_tags"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
@@ -29,7 +37,7 @@ ActiveRecord::Schema[7.1].define(version: 2024_06_26_145115) do
 
   create_table "venues", force: :cascade do |t|
     t.string "name"
-    t.string "description"
+    t.text "description"
     t.integer "rows"
     t.integer "seats_per_row"
     t.datetime "created_at", null: false
