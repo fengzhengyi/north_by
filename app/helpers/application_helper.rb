ef23fb48c12d4b26586eb9caf007e637906380e5
fengzhengyi <512369179@qq.com>
2024-07-05 06:12:43 +0800

    svg can be setted clase by use embedded_svg helper

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,16 @@
 module ApplicationHelper
+  def embedded_svg(filename, options = {})
+    asset = Rails.application.assets.find_asset(filename)
+
+    if asset
+      file = asset.source.force_encoding('UTF-8')
+      doc = Nokogiri::HTML::DocumentFragment.parse file
+      svg = doc.at_css 'svg'
+      svg['class'] = options[:class] if options[:class].present?
+    else
+      "<!-- SVG #{filename} not found -->"
+    end
+
+    raw doc
+  end
 end
