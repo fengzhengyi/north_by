02c64ab1e598fe7702417215ece0128b97bcefd2
fengzhengyi <512369179@qq.com>
2024-07-25 09:18:51 +0800

    favorite toggle controller

--- app/javascript/controllers/favorite_toggle_controller.js
+++ app/javascript/controllers/favorite_toggle_controller.js
@@ -1,9 +1,34 @@
-import { Controller } from "@hotwired/stimulus"
+import {Controller} from "@hotwired/stimulus"
 
 // Connects to data-controller="favorite-toggle"
 export default class extends Controller {
-  static targets = ['elementToHide']
-  toggle() {
-    this.elementToHideTarget?.classList.toggle('hidden')
-  }
+    static targets = ['elementToHide', 'elementWithText']
+    static  values = ['visible']
+
+    toggle() {
+        this.toggleVisible()
+    }
+
+    toggleVisible() {
+        this.visibleValue = !this.visibleValue
+    }
+
+    visibleValueChanged() {
+        window.alert('changed')
+        this.updateHiddenClass()
+        this.updateText()
+    }
+
+    updateHiddenClass() {
+        this.elementToHideTarget?.classList.toggle('hidden', !this.visibleValue)
+    }
+
+    newText() {
+        return this.visibleValue ? 'Hide' : 'Show'
+    }
+
+    updateText() {
+        this.elementWithTextTarget.innerText = this.newText()
+    }
+
 }
